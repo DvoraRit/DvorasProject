df0d8d297f2700160185c90534da88acec7a0598
DvoraRit <dvorarit@gmail.com>
2026-04-05 16:59:43 +0300

    splash screen init

--- App.js
+++ App.js
@@ -1,8 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import React, { useCallback, useEffect, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [animationFinished, setAnimationFinished] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts or API calls here
+        await new Promise(resolve => setTimeout(resolve, 2000)); 
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        // Hide the native static splash and show our GIF
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    prepare();
+  }, []);
+
+  if (!appIsReady || !animationFinished) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/gif/SplashScreen.gif')}
+          style={styles.gif}
+          onLoadEnd={() => {
+            // Optional: Timer to move to the main app after the GIF plays
+            setTimeout(() => setAnimationFinished(true), 3000);
+          }}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +54,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F0F7F9', // Matches the logo background
+  },
+  gif: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
   },
 });
+
